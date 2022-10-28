c5c41c5c3aef66c73a56b59ba5ceb25044f2c287
AnnaNychyk <annanichichka@gmail.com>
2022-10-28 11:53:22 +0300

    done hw3

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -1,17 +1,13 @@
-const contacts = require("../../models/contacts");
+const { Contact } = require("../../models/contact");
 const { RequestError } = require("../../helpers");
 
 const removeContact = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
-    if (!result) {
-      throw RequestError(404, "Contact not found");
-    }
-    res.json({ message: "Contact deleted" });
-  } catch (error) {
-    next(error);
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndDelete(contactId);
+  if (!result) {
+    throw RequestError(404, "Contact not found");
   }
+  res.json({ message: "Contact deleted" });
 };
 
 module.exports = removeContact;
